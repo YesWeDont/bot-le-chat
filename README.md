68f0b7a28b3c6c05a477a1a17e94e24b43e1d60c
YesWeDont <47885665+YesWeDont@users.noreply.github.com>
2022-04-11 14:02:21 +1000

    occasional tts + bot invite link??

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Bot le chat
 A chatbot for discord, made with the `api.affiliateplus.com` API
+You can invite it or host it yourself (see below)
 
-## Prerequisites
+## [Invite link](https://discord.com/api/oauth2/authorize?client_id=962877894911201321&permissions=274877913088&scope=bot)
+"Use TTS" permission is OPTIONAL...
 
-latest versions of [`Node.js`](https://nodejs.org) and [`npm`](https://npmjs.org) (bundled with Node.js)
+**...but the rest aren't**
 
-## Setup
+## Host it yourself
+
+### Prerequisites
+
+Latest versions of [`Node.js`](https://nodejs.org) and [`npm`](https://npmjs.org)(installed with Node.js)
+
+### Setup
 
 Clone the repo
 
@@ -22,3 +30,10 @@ Add discord bot to your server
 RUN `node index`
 
 Tada!
+
+
+### Config
+
+`defaultConfig.json` - refer to [the API's docs](https://api.affiliateplus.xyz/api/docs/chatbot)
+
+`.env` - insert bot token there
--- index.js
+++ index.js
@@ -1,14 +1,51 @@
-function endpoint(message, name, user){return `https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message)}`};
+// @ts-check
+
+// Configure .env file
 require('dotenv').config();
-const djs = new require('discord.js'),
-client = new djs.Client({intents:[djs.Intents.FLAGS.GUILDS, djs.Intents.FLAGS.GUILD_MESSAGES]}),
-fetch = require('node-fetch');
-client.on('ready', ()=>console.log('ready'))
+// DiscordJS: import a client that only cares about server + dm messages
+const djs = require('discord.js'),
+client = new djs.Client({intents:[djs.Intents.FLAGS.DIRECT_MESSAGES, djs.Intents.FLAGS.GUILD_MESSAGES]}),
+// fetch() API polyfill
+fetch = require('node-fetch'),
+{URLSearchParams} = require('url'),
+// config data for chatbot, eg name, age etc
+conf = require('./defaultConfig.json'),
+startTimestamp = Date.now();
+
+// initialise client
+client.on('ready', ()=>{
+    client.user.setPresence({
+        activities: [{
+            name: 'the chat',
+            type:'WATCHING',
+            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        }],
+        status: 'online'
+    })
+    console.log('Bot ready in %d ms', Date.now() - startTimestamp);
+});
+
+//When recieved message
 client.on('messageCreate', async message=>{
     if(message.author.bot) return;
-    let tunnel = await fetch(endpoint(message.content));
+    let tunnel = await fetch(endpoint(
+        message.content
+            .replace(/\<\:([\w\d_]{2,}:\d+)\>/, ":$1:")
+    ));
     let result = await tunnel.json();
     if(result.error) message.reply('Error occured')
-    else message.reply(result.message)
+    else if(result.message) message.reply({
+        content:result.message,
+        tts:(message.channel.permissionsFor ? message.channel.permissionsFor(client.user).has(djs.Permissions.FLAGS.SEND_TTS_MESSAGES) : true) && Math.random() < 0.1,
+        failIfNotExists:false
+    })
 })
 client.login(process.env.TOKEN)
+function endpoint(message){
+    return `https://api.affiliateplus.xyz/api/chatbot?${new URLSearchParams({
+        message: encodeURIComponent(message),
+        ...conf
+    }).toString()}`
+};
+
+console.log("Script running since t=%d", startTimestamp)
