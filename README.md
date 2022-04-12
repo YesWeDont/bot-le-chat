4078baf636b2c7743df9d0c6fb5ab3b0baf3ea3d
YesWeDont <47885665+YesWeDont@users.noreply.github.com>
2022-04-12 14:37:35 +1000

    Presences + Code Audit

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Bot le chat
-A chatbot for discord, made with the `api.affiliateplus.com` API
-You can invite it or host it yourself (see below)
+A chatbot for discord, made with the `api.affiliateplus.com` API.
+You can invite it or host it yourself. (see below)
 
 ## [Invite link](https://discord.com/api/oauth2/authorize?client_id=962877894911201321&permissions=274877913088&scope=bot)
-"Use TTS" permission is OPTIONAL...
 
-**...but the rest aren't**
+Permissions needed:
+|Permission name| Use TTS perm | Send Messages in threads | Send Messages | Know your IP |
+|---|---|---|---|---|
+|Optional|Yes|Yes|Yes if you don't want to use the bot| No unless I tell you otherwise \[jk] |
+|Description | Enable to see a *amusing* and *interesting* effect | Quite obselete but I know some people still use threads | If you don't want to use a bot and just want more members in your server, disable it | Use it so I can DDoS you |
 
 ## Host it yourself
 
@@ -17,7 +20,7 @@ Latest versions of [`Node.js`](https://nodejs.org) and [`npm`](https://npmjs.org
 
 Clone the repo
 
-`npm i`
+Run `npm i`
 
 Go to discord dev portal and get a bot token
 
@@ -27,13 +30,11 @@ Create file `.env` and put in `TOKEN=[token here]`
 
 Add discord bot to your server
 
-RUN `node index`
+Tada! Your bot is all set. Run `node index` to start it up
 
-Tada!
-
-
+### Project Structure
 ### Config
 
 `defaultConfig.json` - refer to [the API's docs](https://api.affiliateplus.xyz/api/docs/chatbot)
 
-`.env` - insert bot token there
+`.env` - insert bot token
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 // Configure .env file
 require('dotenv').config();
 // DiscordJS: import a client that only cares about server + dm messages
@@ -10,42 +8,52 @@ fetch = require('node-fetch'),
 {URLSearchParams} = require('url'),
 // config data for chatbot, eg name, age etc
 conf = require('./defaultConfig.json'),
+// debug
 startTimestamp = Date.now();
 
-// initialise client
+// Init client presence + change presence every 10 mins
 client.on('ready', ()=>{
-    client.user.setPresence({
-        activities: [{
-            name: 'the chat',
-            type:'WATCHING',
-            url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-        }],
-        status: 'online'
-    })
+    presences();
+    setInterval(presences, 10*60*1000);
     console.log('Bot ready in %d ms', Date.now() - startTimestamp);
 });
 
-//When recieved message
+// Call the API whenever recieved message
 client.on('messageCreate', async message=>{
     if(message.author.bot) return;
     let tunnel = await fetch(endpoint(
+        // TODO: Ping sanitization
         message.content
-            .replace(/\<\:([\w\d_]{2,}:\d+)\>/, ":$1:")
+        // Emoji sanitization
+        .replace(/\<\:([\w\d_]{2,}:\d+)\>/, ":$1:")
     ));
+
     let result = await tunnel.json();
+    
     if(result.error) message.reply('Error occured')
     else if(result.message) message.reply({
         content:result.message,
+        // TTS Messages: give people a shock once a while
         tts:(message.channel.permissionsFor ? message.channel.permissionsFor(client.user).has(djs.Permissions.FLAGS.SEND_TTS_MESSAGES) : true) && Math.random() < 0.1,
         failIfNotExists:false
-    })
-})
-client.login(process.env.TOKEN)
+    });
+});
+
+// Login into Discord
+client.login(process.env.TOKEN);
+
+// Helper function to include the chatbot's config into querystring parameters of API request
 function endpoint(message){
     return `https://api.affiliateplus.xyz/api/chatbot?${new URLSearchParams({
         message: encodeURIComponent(message),
         ...conf
-    }).toString()}`
-};
+    }).toString()}`;
+}
+
+// Set presence info. Runs every 10 minutes.
+function presences(){
+    let data = require("./presences.json");
+    client.user.setPresence(data[Math.floor(Math.random()*data.length)])
+}
 
 console.log("Script running since t=%d", startTimestamp)
